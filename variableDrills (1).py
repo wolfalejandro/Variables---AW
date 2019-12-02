82de0af7759b8ab88f35555696331786c45e0fa4
wolfalejandro <57291114+wolfalejandro@users.noreply.github.com>
2019-12-01 23:35:27 -0600

    Add files via upload

---
+++ variableDrills (1).py
@@ -0,0 +1,70 @@
+'''
+For this assignment you should read the task, then below the task do what it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a variable called name and assign it to your name. And print the variable.
+name = "Michael"
+print(name)
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+'''INTEGERS'''
+#1) Make a variable called var1 that is set to 5. And print the variable.
+
+#2) Make a variable called var2 that is set to 10. And print the variable.
+
+#3) Make a variable called var3 that is set to var2. And print the variable.
+
+#4) Make a variable called x that is set to 24. And print the variable.
+
+#5) Make a variable called y that is set to 100. And print the variable.
+
+'''FLOATS'''
+#6) Make a variable called z that is set to 1.1. And print the variable.
+
+#7) Make a variable called a that is set to 2.75. And print the variable.
+
+#8) Make a variable called b that is set to a. And print the variable.
+
+#9) Make a variable called c that is set to 100.1. And print the variable.
+
+#10) Make a variable called d that is set to 12.3. And print the variable.
+
+'''STRINGS'''
+#11) Make a variable called name that is set to your name. And print the variable.
+
+#12) Make a variable called e that is set to the first letter of your name. And print the variable.
+
+#13) Make a variable called f that is set to the last letter of your name. And print the variable.
+
+#14) Make a variable called g that is set to "chicken". And print the variable.
+
+#15) Make a variable called h that is set to the first 3 letters of you name, using the string indexes. And print the variable.
+
+'''BOOLEANS'''
+#16) Make a variable called done that is set to False. And print the variable.
+
+#17) Make a variable called smart that is set to True. And print the variable.
+
+#18) Make a variable called i that is set to done. And print the variable.
+
+#19) Make a variable called j that is set to False. And print the variable.
+
+#20) Make a variable called almostDone that is set to True. And print the variable.
+
+'''LISTS'''
+#21) Make a list called listInt of 5 integer variables.
+
+#22) Print the 3rd item in listInt, using indexes.
+
+#23) Make a list called listString of 5 string variables.
+
+#24) Print the 4th item in listString, using indexes.
+
+#25) Make a list called listBoolean of 5 boolean variables. Then print each item in the list, using indexes.
