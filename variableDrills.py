821cddd74cf0a4123e5360ec82a5f07b18d0c74c
wolfalejandro <57291114+wolfalejandro@users.noreply.github.com>
2019-12-01 23:37:02 -0600

    Add files via upload

---
+++ variableDrills.py
@@ -0,0 +1,97 @@
+'''
+For this assignment you should read the task, then below the task do what it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a variable called name and assign it to your name. And print the variable.
+from turtle import done
+name = "Michael"
+print(name)
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+'''INTEGERS'''
+#1) Make a variable called var1 that is set to 5. And print the variable.
+Var1=5 
+print(Var1)
+#2) Make a variable called var2 that is set to 10. And print the variable.
+var2=10
+print(var2)
+#3) Make a variable called var3 that is set to var2. And print the variable.
+var3=var2
+print(var3)
+#4) Make a variable called x that is set to 24. And print the variable.
+varx=24
+print(varx)
+#5) Make a variable called y that is set to 100. And print the variable.
+vary=100
+print(vary)
+'''FLOATS'''
+#6) Make a variable called z that is set to 1.1. And print the variable.
+varz=1.1
+print(varz)
+#7) Make a variable called a that is set to 2.75. And print the variable.
+vara=2.75
+print(vara)
+#8) Make a variable called b that is set to a. And print the variable.
+varb=vara
+print(varb)
+#9) Make a variable called c that is set to 100.1. And print the variable.
+varc=100.1
+print(varc)
+#10) Make a variable called d that is set to 12.3. And print the variable.
+vard=12.3
+print(vard)
+'''STRINGS'''
+#11) Make a variable called name that is set to your name. And print the variable.
+varname="Alejandro"
+print(varname)
+#12) Make a variable called e that is set to the first letter of your name. And print the variable.
+vare="A"
+print(vare)
+#13) Make a variable called f that is set to the last letter of your name. And print the variable.
+varf="O"
+print(varf)
+#14) Make a variable called g that is set to "chicken". And print the variable.
+varg="chicken"
+print(varg)
+#15) Make a variable called h that is set to the first 3 letters of you name, using the string indexes. And print the variable.
+varh=name[0:3]
+print(varh)
+'''BOOLEANS'''
+#16) Make a variable called done that is set to False. And print the variable.
+vardone=False
+print(vardone)
+#17) Make a variable called smart that is set to True. And print the variable.
+varsmart=True
+print(varsmart)
+#18) Make a variable called i that is set to done. And print the variable.
+i=done
+print(i)
+#19) Make a variable called j that is set to False. And print the variable.
+varj=False
+print(varj)
+#20) Make a variable called almostDone that is set to True. And print the variable.
+varalmostDone=True
+print(varalmostDone)
+'''LISTS'''
+#21) Make a list called listInt of 5 integer variables.
+listInt=[1,2,3,4,5]
+#22) Print the 3rd item in listInt, using indexes.
+print(listInt[2])
+#23) Make a list called listString of 5 string variables.
+ListString=["a","b","c","d","e"]
+#24) Print the 4th item in listString, using indexes.
+print(ListString[3])
+#25) Make a list called listBoolean of 5 boolean variables. Then print each item in the list, using indexes.
+listBoolean=[True,False,True,False,False]
+print(listBoolean[0])
+print(listBoolean[1])
+print(listBoolean[2])
+print(listBoolean[3])
+print(listBoolean[4])
