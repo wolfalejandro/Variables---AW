bdcd4fe420b32f4a7e1ed8fe7110d1a6126a0c09
wolfalejandro <57291114+wolfalejandro@users.noreply.github.com>
2019-12-01 23:41:09 -0600

    Add files via upload

---
+++ debugVariableDrills.py
@@ -0,0 +1,115 @@
+'''
+For this assignment you should look at the variables create below and find the error.
+For each task, there will be one error that you must find and correct.
+Sometimes there will be an explanation of the problem and/or tips that can help you
+complete the tasks.
+EXAMPLE TASK:
+'''
+#EX)
+#Broken:
+#name == "Michael"
+#print(name)
+#Correct:
+name = "Michael"
+print(name)
+'''
+END EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1)
+#Broken:
+#name == "Alejandro"
+#print(name)
+#Correct:
+name = "Alejandro"
+print(name)
+
+#2)
+#Broken:
+#x = "Alejandro"
+#print(x)
+#Correct:
+x= "Alejandro"
+print(x)
+#3)
+#Broken:
+#yay = "yay"
+#print(aya)
+#PROBLEM: The variable is created correctly, but it won't print.
+#TIP: The variable isn't the problem
+#Correct:
+yay="yay"
+print(yay)
+#4)
+#Broken:
+#y == 4
+#print(y)
+#Correct:
+y=4
+print(y)
+#5)
+#Broken:
+#m == 5
+#print(m)
+#Correct:
+m=5
+print(m)
+'''
+For the next section, there will be a variable create correctly. Your job will
+be to write what kind of variable it is.
+EXAMPLE:
+'''
+#EX)
+#name = "michael"
+#ANSWER: string
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+#1)
+#x = 1.01
+#ANSWER: float
+
+#2)
+#y = 2
+#ANSWER:integer
+
+#3)
+#a = True
+#ANSWER: boolean
+
+#4)
+#b = 1
+#ANSWER: integer
+
+#5)
+#c = "string"
+#ANSWER: string
+
+#6)
+#d = False
+#ANSWER: boolean
+
+#7)
+#e = ["a", "b", "c"]
+#ANSWER:listString
+
+#8)
+#f = [1, 2, 3]
+#ANSWER: listInt
+
+#9)
+#g = "nope"
+#ANSWER: string
+
+#10)
+#h = 1.24
+#ANSWER: float
